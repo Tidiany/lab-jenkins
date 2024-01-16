678f9bdfb57d5728c3f6e663c97810cec777fe02
Tidiany <tidianycorporation337@gmail.com>
2024-01-16 13:02:20 +0000

    Upload code...

---
+++ src/main/java/com/groupeisi/labjenkins/LabJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.labjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupeisi/labjenkins/LabJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.labjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
